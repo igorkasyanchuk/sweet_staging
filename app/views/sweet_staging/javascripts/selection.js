f95eed6d77c2c0265b0415a5c7f132dd1ea46548
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2021-04-22 10:12:08 -0700

    fix for text colorization

--- app/views/sweet_staging/javascripts/selection.js
+++ app/views/sweet_staging/javascripts/selection.js
@@ -6,10 +6,20 @@ function getSelectionText(){
   return selectedText;
 }
 
-document.addEventListener('mouseup', function(){
+function copyHandler() {
   var thetext = getSelectionText()
   if (thetext.length > 0) { // check there's some text selected
       console.log(thetext);
       document.execCommand("copy"); // logs whatever textual content the user has selected on the page
   }
-}, false);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (!!document.getElementById('console')) {
+    document.getElementById('console').addEventListener('mouseup', copyHandler, false);
+  }
+
+  if (!!document.getElementById('logs')) {
+    document.getElementById('logs').addEventListener('mouseup', copyHandler, false);
+  }
+});
